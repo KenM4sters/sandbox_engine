7bd555dad4f6bc25dc8353f4af0d2fbafdb9a8c5
KenM4sters <EHCKenway@outlook.com>
2024-03-07 00:14:59 +0000

    added a floor - happy with how objects are implemented for now - next will be postprocessing

--- src/mesh/mesh.cpp
+++ src/mesh/mesh.cpp
@@ -20,8 +20,8 @@ void Mesh::InitGeometry() {
 void Mesh::InitTransforms() {
     transform_.projection = glm::perspective(camera_->zoom_, scr_wdith_ / scr_height_, 0.1f, 100.0f);
     transform_.view = camera_->GetViewMatrix();
-    transform_.model = glm::scale(transform_.model, scale_);
     transform_.model = glm::translate(transform_.model, position_);
+    transform_.model = glm::scale(transform_.model, scale_);
     transform_.model = glm::rotate(transform_.model, rotation_.first, rotation_.second);
 }
 
@@ -29,8 +29,8 @@ void Mesh::UpdateTransforms() {
     transform_.view = camera_->GetViewMatrix();
 
     transform_.model = glm::mat4(1.0f);
-    transform_.model = glm::scale(transform_.model, scale_);
     transform_.model = glm::translate(transform_.model, position_);
+    transform_.model = glm::scale(transform_.model, scale_);
     transform_.model = glm::rotate(transform_.model, rotation_.first, rotation_.second);
 
     material_->GetShaderMaterial()->Use();
--- src/scene/scene.cpp
+++ src/scene/scene.cpp
@@ -19,24 +19,36 @@ void Scene::AddMesh(Mesh* mesh, std::string name) {
 }
 
 void Scene::Init() {
-    auto shader = shader_res_->AddResource("src/shaders/cube_1.vert", "src/shaders/cube_1.frag", nullptr, "cube1");
-    auto shader_2 = shader_res_->AddResource("src/shaders/cube_2.vert", "src/shaders/cube_2.frag", nullptr, "cube2");
+    auto cube_1_shader= shader_res_->AddResource("src/shaders/cube_1.vert", "src/shaders/cube_1.frag", nullptr, "cube1");
+    auto cube_2_shader = shader_res_->AddResource("src/shaders/cube_2.vert", "src/shaders/cube_2.frag", nullptr, "cube2");
+    auto floor_shader = shader_res_->AddResource("src/shaders/floor.vert", "src/shaders/floor.frag", nullptr, "floor");
 
     // Cube 1
-    Material* material = new Material(shader, std::string("cube1"));
-    material->SetColor(glm::vec3(0.3f, 0.9f, 1.0f));
-    UBufferGeometry* geo = new UBufferGeometry(SANDBOX_CUBE);
-    Mesh* mesh = new Mesh(material, geo, std::tuple<Camera*, float, float>(camera_, scr_width_, scr_height_));
+    Material* cube_1_mat = new Material(cube_1_shader, std::string("cube1"));
+    cube_1_mat->SetColor(glm::vec3(0.3f, 0.9f, 1.0f));
+    UBufferGeometry* cube_1_geo = new UBufferGeometry(SANDBOX_CUBE);
+    Mesh* mesh = new Mesh(cube_1_mat, cube_1_geo, std::tuple<Camera*, float, float>(camera_, scr_width_, scr_height_));
     AddMesh(mesh, "cube1");
     mesh->SetPosition(glm::vec3(5.0f, 0.0f, -10.0f));
 
     // Cube 2
-    Material* mat_2 = new Material(shader_2, std::string("cube2"));
-    mat_2->SetColor(glm::vec3(0.9f, 0.1f, 0.0f));
-    UBufferGeometry* geo_2 = new UBufferGeometry(SANDBOX_CUBE);
-    Mesh* mesh_2 = new Mesh(mat_2, geo_2, std::tuple<Camera*, float, float>(camera_, scr_width_, scr_height_));
+    Material* cube_2_mat = new Material(cube_2_shader, std::string("cube2"));
+    cube_2_mat->SetColor(glm::vec3(0.9f, 0.1f, 0.0f));
+    UBufferGeometry* cube_2_geo = new UBufferGeometry(SANDBOX_CUBE);
+    Mesh* mesh_2 = new Mesh(cube_2_mat, cube_2_geo, std::tuple<Camera*, float, float>(camera_, scr_width_, scr_height_));
     AddMesh(mesh_2, "cube2");
     mesh_2->SetPosition(glm::vec3(-5.0f, 0.0f, -10.0f));
+
+    // Floor
+    Material* floor_mat = new Material(floor_shader, std::string("floor"));
+    floor_mat->SetColor(glm::vec3(0.5f, 0.5f, 0.5f));
+    UBufferGeometry* floor_geo = new UBufferGeometry(SANDBOX_SQUARE);
+    Mesh* floor_mesh = new Mesh(floor_mat, floor_geo, std::tuple<Camera*, float, float>(camera_, scr_width_, scr_height_));
+    AddMesh(floor_mesh, "floor");
+    floor_mesh->SetPosition(glm::vec3(0.0f, -1.0f, 0.0f));
+    floor_mesh->SetScale(50.0f);
+    floor_mesh->SetRotation(glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
 }
 
 void Scene::Render(float &delta_time) {
@@ -45,8 +57,6 @@ void Scene::Render(float &delta_time) {
     for(const auto& mesh : mesh_repo_) {
         auto geo = mesh.second->GetGeometry();
         mesh.second->GetMaterial()->GetShaderMaterial()->Use();
-
-        mesh.second->SetRotation((float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f));
         mesh.second->UpdateTransforms();
 
         glBindVertexArray(mesh.second->VAO_);
