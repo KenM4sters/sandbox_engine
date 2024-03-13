6f7ff501a7387b611b7bdd1abbaa8dd05de6235e
KenM4sters <EHCKenway@outlook.com>
2024-03-13 09:49:19 +0000

    added floor back - working on importing models, skeletal motion and collision detection

--- src/systems/objects/objects.cpp
+++ src/systems/objects/objects.cpp
@@ -45,10 +45,19 @@ void SObjects::Init() {
         -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
     };
+    // Mario Cube
     Shader* cube_shader = shaders_->GetResource("cube");
     Texture2D* mario_tex = textures_->GetResource("mario");
     Mesh* cube_mesh = new Mesh(new BufferGeometry(vertices, SANDBOX_CUBE_VERTICES_COUNT), cube_shader, mario_tex);
     children_["cube"] = cube_mesh;
+
+    // Floor
+    Shader* floor_shader = shaders_->GetResource("floor");
+    Texture2D* sand_tex = textures_->GetResource("sand");
+    Mesh* floor_mesh = new Mesh(new BufferGeometry(vertices, SANDBOX_CUBE_VERTICES_COUNT), floor_shader, sand_tex);
+    floor_mesh->transforms_.position = glm::vec3(0.0f, -0.75f, 0.0f);
+    floor_mesh->transforms_.scale = glm::vec3(200.0f, 0.25f, 200.0f);
+    children_["floor"] = floor_mesh;
 }
 
 void SObjects::Draw() {
